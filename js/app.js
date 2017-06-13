d8b62a889912af1c51f478a1681a4124e2cd73d0
lewis <16711731@protonmail.com>
2017-06-13 20:42:16 +0100

    Refactored circle generation prototype methods

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 
 class Game {
+
     constructor(args) {
         this.stage = document.getElementById('game-stage');
         this.circle = new Circle(this.stage);
     }
+
 }
 
 class Circle {
@@ -13,22 +15,28 @@ class Circle {
         this.x = xpos;
         this.y = ypos;
         this.type = type;
-        this.mapPosition();
+        this.render();
     }
 
-    mapPosition() {
+    _mapCoordinates() {
         if (this.x === -1 || this.y === -1) {
             this.x = Math.random();
             this.y = Math.random();
         }
 
+        this.x *= 1000;
+        this.y *= 1000;
+    }
+
+    render() {
+        this._mapCoordinates();
         let div = document.createElement('div');
         div.classList.add('circle');
-        div.style.transform = "translateX(" + this.x * 1000
-            + "%) translateY(" + this.y * 1000 + "%)";
+        div.style.transform = "translateX(" + this.x
+            + "%) translateY(" + this.y + "%)";
         this.stage.prepend(div);
-        console.log(div.style.transform);
     }
+
 }
 
 window.addEventListener('load', () => new Game());
